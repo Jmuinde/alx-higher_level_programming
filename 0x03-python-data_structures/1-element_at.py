5b7fd40cb53d85c4f6e319b546f6df16ddb08dcd
Jmuinde <jmuindedt@gmail.com>
2024-03-21 08:05:47 -0700

    add

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,7 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0 or  idx > (len(my_list) - 1):
+     if idx > (len(my_list) - 1) or idx < 0:
         return None
     else:
         return my_list[idx]
-
