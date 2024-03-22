21928abdb6d641b1b18479f0458070b38901262c
Jmuinde <jmuindedt@gmail.com>
2024-03-22 02:42:51 -0700

    del

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@
 def no_c(my_string):
     string = ""
     for j in my_string:
-        if j != 'c' and j !=  'C':
+        if j is not 'c' and j is not 'C':
             string += j
     return string
