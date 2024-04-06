65309d165f87e7e7a5b76f57fc0b4ef6c719075b
Jmuinde <jmuindedt@gmail.com>
2024-04-06 04:46:57 -0700

    update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,4 +6,6 @@ def search_replace(my_list, search, replace):
     for j in range(len(updated_list)):
         if updated_list[j] == search:
             updated_list[j] = replace
+        else:
+            updated_list = my_list
     return (updated_list)
