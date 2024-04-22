304dc6119e9a0ea01d0969ded48117768b3233b6
Jmuinde <jmuindedt@gmail.co>
2024-04-22 15:09:40 +0000

    divide

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    try:
+       out_put = a / b
+    except ZeroDivisionError:
+        out_put = None
+    finally:
+        print("Inside result: {}".format(out_put))
+        return out_put
