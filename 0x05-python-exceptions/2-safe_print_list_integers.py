abb577188926e67ca21f55c4c169a733e23521c4
Jmuinde <jmuindedt@gmail.co>
2024-04-22 14:51:10 +0000

    enum

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     enum = 0
     for k in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[k]), end="")
             enum += 1
         except (TypeError, ValueError):
             continue
