84308adf41dfa9c291b55b30b7b4ff8c449c68b0
Jmuinde <jmuindedt@gmail.co>
2024-08-07 14:42:30 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" first bit of creating the class"""
+"""first bit of creating the class."""
 
 
 class Square:
-	""" the square class """
+	"""the square class."""
 	def __init__(self, size=0):
 		if type(size) != int:
 			raise TypeError("size must be an integer")
